daee8c5a9c26f91441534612cbe1addf1671d397
mdadasdasd <43049018+mdadasdasd@users.noreply.github.com>
2018-09-06 23:28:54 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,143 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '$'
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Crystal The best`,"http://twitch.tv/Crystal")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN);
+
+
+
+client.on('message', message => {
+    if (message.content.startsWith(prefix + 'help')) {
+    const embed = new Discord.RichEmbed() 
+        .setColor("#000000")
+        .setDescription(`
+               -=- اوامر بورد كاست -=-
+  ❖$bc [message] | بورد كاست للكل و مطور
+  ❖$2bc [message] | بورد كاست للكل غير مطور
+  ❖$3bc [message] | بورد كاست للونلاين غير مطور
+  ❖$4bc [message] | بورد كاست للونلاين مطور
+  ❖$role-bc @rank [message] | رساله لكل من يملك الرتبه الممشنه
+  `)
+     message.author.sendEmbed(embed)
+     message.reply('تم ارسالك اوامر البورد كاست بلخاص')
+    }
+});
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`); 
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+		var bc = new Discord.RichEmbed()
+			.addField(':earth_americas:  » سيرفر : ', message.guild.name)
+			.addField(':thinking:  » راسل : ', message.author.username)
+            .addField(':pencil:  » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
+
+
+client.on('message', message => {
+if (message.author.id === client.user.id) return;
+if (message.guild) {
+let embed = new Discord.RichEmbed()
+let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + '2bc') {
+if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+if (!args[1]) {
+return;
+}
+  message.guild.members.forEach(m => {
+if(!message.member.hasPermission('ADMINISTRATOR')) return;
+      var bc = new Discord.RichEmbed()
+      m.sendMessage(args)
+  });
+         if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply(":x: **ليس لديك صلاحية للنشر هنا**");
+         message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`); 
+
+}
+} else {
+  return;
+}
+});
+
+
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "3bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+};     
+});
+
+
+
+
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "4bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+      var bc = new Discord.RichEmbed()
+	  		.addField(':earth_americas:  » سيرفر : ', message.guild.name)
+			.addField(':thinking:  » راسل : ', message.author.username)
+            .addField(':pencil:  » الرسالة : ', args)
+			            m.send(`${m}`,{embed: bc});
+  })
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+};     
+});
